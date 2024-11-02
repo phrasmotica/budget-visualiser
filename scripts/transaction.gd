527b50d81ac24215a9dca3e1a466b58ba0ef818c
Julian L <julian.lawrance@gmail.com>
2024-11-02 22:41:47 +0000

    Simplify logic for notifying that a transaction's property has changed

--- scripts/transaction.gd
+++ scripts/transaction.gd
@@ -8,10 +8,22 @@ var id: int = -1
 var name := "":
     set(value):
         name = value
-        emit_changed()
+
+        handle_property_change_from_editor()
 
 @export
 var amount: float = 0:
     set(value):
         amount = value
+
+        handle_property_change_from_editor()
+
+func handle_property_change_from_editor() -> void:
+    if Engine.is_editor_hint():
+        emit_changed()
+
+func set_transaction_name(new_name: String, emit: bool) -> void:
+    name = new_name
+
+    if emit:
         emit_changed()
--- scripts/transaction_input.gd
+++ scripts/transaction_input.gd
@@ -24,8 +24,6 @@ var amount_edit: MoneyEdit = %AmountEdit
 @onready
 var delete_button: Button = %DeleteButton
 
-var prevent_callback := false
-
 signal adjust_transaction(transaction: Transaction)
 signal delete_transaction(transaction: Transaction)
 
@@ -59,19 +57,16 @@ func update_delete_mode():
 		delete_button.visible = delete_mode
 
 func handle_transaction_changed():
-	if not prevent_callback:
-		update_name()
-		update_amount()
+	update_name()
+	update_amount()
 
-		adjust()
+	adjust()
 
 func _on_name_edit_text_changed(new_text: String) -> void:
-	prevent_callback = true
-	transaction.name = new_text
-	prevent_callback = false
+	transaction.set_transaction_name(new_text, false)
 
 func _on_name_edit_text_submitted(new_text: String) -> void:
-	transaction.name = new_text
+	transaction.set_transaction_name(new_text, true)
 
 	adjust()
 
@@ -79,9 +74,7 @@ func _on_name_edit_focus_exited() -> void:
 	adjust()
 
 func _on_amount_edit_amount_changed(x: float) -> void:
-	prevent_callback = true
 	transaction.amount = x
-	prevent_callback = false
 
 	adjust()
 
