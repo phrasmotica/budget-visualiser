6424769090bfd8598798254537cacb467e0434b1
Julian L <julian.lawrance@gmail.com>
2024-11-16 14:29:17 +0000

    Create scene that can flip between a check box and a delete button

---
+++ scripts/check_box_or_delete_button.gd
@@ -0,0 +1,42 @@
+@tool
+class_name CheckBoxOrDeleteButton extends HBoxContainer
+
+@export
+var checked := false:
+	set(value):
+		checked = value
+
+		update_checkbox()
+
+@export
+var delete_mode := false:
+	set(value):
+		delete_mode = value
+
+		update_mode()
+
+@onready
+var delete_button: Button = %DeleteButton
+
+@onready
+var check_box: Button = %CheckBox
+
+signal check_box_pressed(checked: bool)
+signal delete_button_pressed
+
+func update_checkbox():
+	if check_box:
+		check_box.button_pressed = checked
+
+func update_mode():
+	if delete_button:
+		delete_button.visible = delete_mode
+
+	if check_box:
+		check_box.visible = not delete_mode
+
+func _on_check_box_pressed() -> void:
+	check_box_pressed.emit(check_box.button_pressed)
+
+func _on_delete_button_pressed() -> void:
+	delete_button_pressed.emit()
