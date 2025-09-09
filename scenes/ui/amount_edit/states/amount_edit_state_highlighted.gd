6e87e2249526d0f4c111be38861ad37b4b9c2aa6
Julian Lawrance <julian.lawrance@gmail.com>
2025-09-09 19:57:19 +0100

    Allow changing an amount by 10 instead of 1 when the Shift key is held down

--- scenes/ui/amount_edit/states/amount_edit_state_highlighted.gd
+++ scenes/ui/amount_edit/states/amount_edit_state_highlighted.gd
@@ -2,6 +2,7 @@ class_name AmountEditStateHighlighted
 extends AmountEditState
 
 var _amount := 0
+var _is_bulk := false
 
 func _enter_tree() -> void:
 	print("%s is now highlighted" % _amount_edit.name)
@@ -18,15 +19,25 @@ func _enter_tree() -> void:
 		_on_move_down
 	)
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("bulk_modifier"):
+		_is_bulk = true
+
+	if Input.is_action_just_released("bulk_modifier"):
+		_is_bulk = false
+
 func _on_move_up() -> void:
-	_amount = mini(99, _amount + 1)
+	_amount = mini(99, _amount + _get_change_amount())
 
 	_appearance.set_amount(_amount)
 
 func _on_move_down() -> void:
-	_amount = maxi(0, _amount - 1)
+	_amount = maxi(0, _amount - _get_change_amount())
 
 	_appearance.set_amount(_amount)
+
+func _get_change_amount() -> int:
+	return 10 if _is_bulk else 1
 
 func disable() -> void:
 	transition_state(AmountEdit.State.IDLE)
