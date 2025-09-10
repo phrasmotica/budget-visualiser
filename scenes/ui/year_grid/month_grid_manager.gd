a20b0e1e613d68bca7fb56541975a2832e95c105
Julian Lawrance <julian.lawrance@gmail.com>
2025-09-10 22:25:20 +0100

    Ensure the highlighted MonthGrid is idle while the AmountEntryModal is shown

--- scenes/ui/year_grid/month_grid_manager.gd
+++ scenes/ui/year_grid/month_grid_manager.gd
@@ -4,9 +4,20 @@ extends Node
 @export
 var month_grids: Array[MonthGrid] = []
 
+var _highlighted_index := -1
+
 func highlight(index: int) -> void:
+	_highlighted_index = index
+
+	_refresh()
+
+func pause() -> void:
 	for i in month_grids.size():
-		if i == index:
+		month_grids[i].unhighlight()
+
+func _refresh() -> void:
+	for i in month_grids.size():
+		if i == _highlighted_index:
 			month_grids[i].highlight()
 		else:
 			month_grids[i].unhighlight()
--- scenes/ui/year_grid/states/year_grid_state_disabled.gd
+++ scenes/ui/year_grid/states/year_grid_state_disabled.gd
@@ -4,6 +4,8 @@ extends YearGridState
 func _enter_tree() -> void:
 	print("%s is now disabled" % _year_grid.name)
 
+	_month_grid_manager.pause()
+
 	SignalHelper.once(
 		AmountEvents.entry_finished,
 		_on_entry_finished
