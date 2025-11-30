565deda20398258ef6251c7c443fb11d9572d036
Julian L <julian.lawrance@gmail.com>
2025-11-30 22:17:15 +0000

    Start creating month and category resources. Press the Esc key in the spreadsheet UI to add a test transaction to the budget

--- scenes/autoloads/budget_provider.gd
+++ scenes/autoloads/budget_provider.gd
@@ -1,7 +1,23 @@
 extends Node
 
+const BUDGET_CATEGORIES: Array[BudgetCategory] = [
+	preload("res://resources/data/budget_category_groceries.tres"),
+	preload("res://resources/data/budget_category_transport.tres"),
+	preload("res://resources/data/budget_category_drinksout.tres"),
+	preload("res://resources/data/budget_category_mealsout.tres"),
+	preload("res://resources/data/budget_category_pool.tres"),
+]
+
+const BUDGET_MONTHS: Array[BudgetMonth] = [
+	preload("res://resources/data/budget_month_jan.tres"),
+	preload("res://resources/data/budget_month_feb.tres"),
+	preload("res://resources/data/budget_month_mar.tres"),
+	preload("res://resources/data/budget_month_apr.tres"),
+]
+
 var _data: BudgetData = null
 
+signal budget_changed(data: BudgetData)
 signal transaction_added(transaction: BudgetTransaction)
 
 func set_budget_data(data: BudgetData) -> void:
@@ -14,3 +30,10 @@ func add_transaction(
 ) -> void:
 	var new_transaction = _data.add_transaction(category, month, amount)
 	transaction_added.emit(new_transaction)
+
+	budget_changed.emit(_data)
+
+func add_transaction_debug(
+	amount: float,
+) -> void:
+	add_transaction(BUDGET_CATEGORIES.pick_random(), BUDGET_MONTHS[0], amount)
--- scenes/ui/spreadsheet_ui/states/spreadsheet_ui_state_idle.gd
+++ scenes/ui/spreadsheet_ui/states/spreadsheet_ui_state_idle.gd
@@ -10,13 +10,36 @@ func _enter_tree() -> void:
 
 	SignalHelper.once_next_frame(_inject_entered_amount)
 
+	BudgetProvider.set_budget_data(BudgetData.new())
+
+	SignalHelper.persist(
+		BudgetProvider.transaction_added,
+		_on_transaction_added
+	)
+
 func _process(_delta: float) -> void:
-	if Input.is_action_just_released("ui_accept"):
-		_show_modal()
+	if Input.is_action_just_released("ui_cancel"):
+		_add_transaction()
+
+	# if Input.is_action_just_released("ui_accept"):
+	# 	_show_modal()
 
 func _inject_entered_amount() -> void:
 	var entered_amount := _state_data.get_entered_amount()
 	_year_grid.inject_amount(entered_amount)
+
+func _on_transaction_added(transaction: BudgetTransaction) -> void:
+	print("New transaction: %s %s, £%.2f" % [
+		transaction.month.name,
+		transaction.category.name,
+		transaction.amount,
+	])
+
+func _add_transaction() -> void:
+	# [1.0, 5.0]
+	var amount := 4.0 * randf() + 1.0
+
+	BudgetProvider.add_transaction_debug(amount)
 
 func _show_modal() -> void:
 	_amount_entry_modal.enable(0.0)
