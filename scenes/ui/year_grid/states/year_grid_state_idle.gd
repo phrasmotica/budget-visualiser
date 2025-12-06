305d39e6b9d2b740f31ba903fd0ad42f94dc5b79
Julian L <julian.lawrance@gmail.com>
2025-12-06 19:34:12 +0000

    Fully fix YearGrid horizontal scrolling

--- scenes/ui/year_grid/states/year_grid_state_idle.gd
+++ scenes/ui/year_grid/states/year_grid_state_idle.gd
@@ -1,6 +1,9 @@
 class_name YearGridStateIdle
 extends YearGridState
 
+var _needs_to_scroll_left := false
+var _needs_to_scroll_right := false
+
 func _enter_tree() -> void:
 	print("%s is now idle" % _year_grid.name)
 
@@ -34,9 +37,11 @@ func update_budget(data: BudgetData) -> void:
 	_month_grid_manager.inject_budget(data)
 
 func _on_move_right() -> void:
+	_needs_to_scroll_right = true
 	_month_grid_manager.highlight(_index_tracker.next())
 
 func _on_move_left() -> void:
+	_needs_to_scroll_left = true
 	_month_grid_manager.highlight(_index_tracker.previous())
 
 func _on_highlighted_grid_changed(grid: MonthGrid) -> void:
@@ -46,11 +51,15 @@ func _on_highlighted_grid_changed(grid: MonthGrid) -> void:
 	if _year_grid.scroll_horizontal <= left and _year_grid.scroll_horizontal + _year_grid.size.x >= right:
 		return
 
-	# TODO: improve this scrolling behaviour further
-	var scroll_amount_left := left - _year_grid.scroll_horizontal
-	var scroll_amount_right := right - _year_grid.scroll_horizontal
+	var year_grid_end := int(_year_grid.scroll_horizontal + _year_grid.size.x)
+	var scroll_amount_end := right - year_grid_end
 
-	if scroll_amount_left < scroll_amount_right:
+	if _needs_to_scroll_right:
+		_needs_to_scroll_right = false
+
+		_year_grid.scroll_horizontal += scroll_amount_end
+
+	if _needs_to_scroll_left:
+		_needs_to_scroll_left = false
+
 		_year_grid.scroll_horizontal = left
-	else:
-		_year_grid.scroll_horizontal = right
