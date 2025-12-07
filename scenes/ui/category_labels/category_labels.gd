27b552e65f0fe24ea895e396b822d1322776a671
Julian Lawrance <julian.lawrance@gmail.com>
2025-12-07 12:06:27 +0000

    Create scene for rendering labels for a list of categories

---
+++ scenes/ui/category_labels/category_labels.gd
@@ -0,0 +1,37 @@
+@tool
+extends VBoxContainer
+
+const LABEL_MIN_HEIGHT := 60.0
+const LABEL_FONT_SIZE := 32
+
+@export
+var categories: Array[BudgetCategory] = []:
+	set(value):
+		categories = value
+
+		call_deferred("_refresh")
+
+func _refresh() -> void:
+	var child_count := get_child_count()
+
+	for i in categories.size():
+		var new_text := "<Category%d>" % i
+
+		var category := categories[i]
+		if category:
+			new_text = category.name
+
+		var label: Label
+
+		if i >= child_count:
+			label = CategoryLabelFactory.create_label(i)
+
+			add_child(label)
+			label.owner = self
+		else:
+			label = get_child(i) as Label
+
+		label.text = new_text
+
+	while get_child_count() > categories.size():
+		remove_child(get_child(categories.size()))
