ffb39b62491de30467fda147184b1a9d2dd54194
Julian Lawrance <julian.lawrance@gmail.com>
2025-12-10 23:24:30 +0000

    Show sum of recent transaction amounts

--- scenes/ui/recent_transactions/recent_transactions.gd
+++ scenes/ui/recent_transactions/recent_transactions.gd
@@ -11,21 +11,45 @@ var month: BudgetMonth
 var visible_count := 5
 
 @onready
+var none_label: Label = %NoneLabel
+
+@onready
+var ledger: VBoxContainer = %Ledger
+
+@onready
 var transactions_label: Label = %TransactionsLabel
 
 @onready
+var more_section: VBoxContainer = %MoreSection
+
+@onready
 var more_label: Label = %MoreLabel
+
+@onready
+var total_label: Label = %TotalLabel
 
 func _ready() -> void:
 	reload()
 
 func reload() -> void:
 	var transactions := _get_transactions()
+	var has_transactions := transactions.size() > 0
+
+	none_label.visible = not has_transactions
+	ledger.visible = has_transactions
+
 	transactions_label.text = _compute_transactions_text(transactions)
 
 	var more_count := maxi(0, transactions.size() - visible_count)
-	more_label.visible = more_count > 0
 	more_label.text = "+%d more" % more_count
+
+	more_section.visible = more_count > 0
+
+	var total_amount: float = transactions \
+		.map(func(t: BudgetTransaction): return t.amount) \
+		.reduce(Math.sum, 0.0)
+
+	total_label.text = "%.2f" % total_amount
 
 func _get_transactions() -> Array[BudgetTransaction]:
 	var budget_data := BudgetProvider.get_budget_data()
