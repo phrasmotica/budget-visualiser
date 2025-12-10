a56bb92b735047eb49dc27c54ba48da18a45659c
Julian Lawrance <julian.lawrance@gmail.com>
2025-12-10 22:33:13 +0000

    Render the most recent N transactions in a single label

--- scenes/ui/recent_transactions/recent_transactions.gd
+++ scenes/ui/recent_transactions/recent_transactions.gd
@@ -11,6 +11,9 @@ var month: BudgetMonth
 var visible_count := 5
 
 @onready
+var transactions_label: Label = %TransactionsLabel
+
+@onready
 var more_label: Label = %MoreLabel
 
 func _ready() -> void:
@@ -18,7 +21,11 @@ func _ready() -> void:
 
 func reload() -> void:
 	var transactions := _get_transactions()
-	more_label.text = "+%d more" % transactions.size()
+	transactions_label.text = _compute_transactions_text(transactions)
+
+	var more_count := maxi(0, transactions.size() - visible_count)
+	more_label.visible = more_count > 0
+	more_label.text = "+%d more" % more_count
 
 func _get_transactions() -> Array[BudgetTransaction]:
 	var budget_data := BudgetProvider.get_budget_data()
@@ -26,3 +33,13 @@ func _get_transactions() -> Array[BudgetTransaction]:
 		return []
 
 	return budget_data.get_transactions_for_category_and_month(category, month)
+
+func _compute_transactions_text(transactions: Array[BudgetTransaction]) -> String:
+	if transactions.size() <= 0:
+		return ""
+
+	return transactions \
+		.slice(0, visible_count) \
+		.map(func(t: BudgetTransaction): return t.amount) \
+		.map(func(f: float): return "%.2f" % f) \
+		.reduce(Strings.join("\n"))
