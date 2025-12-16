d3b03d6d6530136951f3f5ecf7b259641cceaabc
Julian L <julian.lawrance@gmail.com>
2025-12-16 17:13:48 +0000

    Fix size of YearGrid

--- scenes/ui/year_grid/year_grid_appearance.gd
+++ scenes/ui/year_grid/year_grid_appearance.gd
@@ -12,7 +12,7 @@ var v_scroll_container: ScrollContainer
 var category_scroll_container: ScrollContainer
 
 func for_idle() -> void:
-	h_scroll_container.horizontal_scroll_mode = ScrollContainer.SCROLL_MODE_AUTO
+	h_scroll_container.horizontal_scroll_mode = ScrollContainer.SCROLL_MODE_SHOW_NEVER
 
 func scroll_down_to_highlighted_cell(cell: CategoryCell) -> void:
 	var new_scroll := VScroll.scroll_down_to(v_scroll_container, cell)
