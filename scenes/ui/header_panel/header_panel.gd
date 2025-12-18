2d075e87480251b39d661b78ad166400e1b101d9
Julian L <julian.lawrance@gmail.com>
2025-12-18 14:19:38 +0000

    Add optional start/end icons to HeaderPanel

--- scenes/ui/header_panel/header_panel.gd
+++ scenes/ui/header_panel/header_panel.gd
@@ -9,8 +9,23 @@ var text := "":
 
 		_refresh()
 
+# TODO: only show the icons when the Ctrl/Cmd key is held down. Create a new
+# autoload for handling modifier input on a per-OS basis...
+@export
+var show_icons := false:
+	set(value):
+		show_icons = value
+
+		_refresh()
+
 @onready
 var label: Label = %Label
+
+@onready
+var left_icon: TextureRect = %LeftIcon
+
+@onready
+var right_icon: TextureRect = %RightIcon
 
 func _ready() -> void:
 	_refresh()
@@ -18,3 +33,9 @@ func _ready() -> void:
 func _refresh() -> void:
 	if label:
 		label.text = text
+
+	if left_icon:
+		left_icon.visible = show_icons
+
+	if right_icon:
+		right_icon.visible = show_icons
