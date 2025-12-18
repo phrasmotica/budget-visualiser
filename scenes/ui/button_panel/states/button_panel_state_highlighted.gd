67c23ea7bf06f3c85c9db496bcb0707cfdd8a77d
Julian L <julian.lawrance@gmail.com>
2025-12-18 23:55:05 +0000

    Ensure a ButtonPanel does not listen to a confirm keypress if it's not highlighted

--- scenes/ui/button_panel/states/button_panel_state_highlighted.gd
+++ scenes/ui/button_panel/states/button_panel_state_highlighted.gd
@@ -6,7 +6,10 @@ func _enter_tree() -> void:
 
 	_button_panel.theme_type_variation = "HighlightedButtonPanel"
 
-	SignalHelper.persist(ConfirmCancelInput.confirm, _button_panel.emit_pressed)
+	SignalHelper.persist(ConfirmCancelInput.confirm, _confirm)
+
+func _confirm() -> void:
+	_button_panel.emit_pressed()
 
 func unhighlight() -> void:
 	transition_state(ButtonPanel.State.IDLE)
