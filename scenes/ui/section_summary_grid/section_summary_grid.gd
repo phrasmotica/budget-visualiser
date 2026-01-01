98bf1fda3bc80044576943c0338082c31f8452c1
Julian L <julian.lawrance@gmail.com>
2026-01-01 23:31:19 +0000

    Add TODO item

--- scenes/ui/section_summary_grid/section_summary_grid.gd
+++ scenes/ui/section_summary_grid/section_summary_grid.gd
@@ -20,6 +20,8 @@ var name_label: Label = %NameLabel
 var _state_factory := SectionSummaryGridStateFactory.new()
 var _current_state: SectionSummaryGridState = null
 
+# TODO: for the income section, show income minus all expenditure in a new row
+
 func _ready() -> void:
 	_refresh()
 
